1b6cba27568ef1ed776cfda9d634ca77084ee4f0
nguyenthiensua <nguyenthiensua@teams.airlab.dev>
2023-12-06 17:42:12 +0700

    Add footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.scss';
 import CreatePost from './pages/CreatePost/CreatePost';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <ShowCode content={mkdStr} /> */}
       {/* <CreatePost /> */}
       <Header />
+      <Footer />
     </div>
   );
 };
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { FaCopyright, FaHandPointRight } from 'react-icons/fa';
+import { Logo } from '../../Logo';
+import './footer.scss';
+import { TiSocialFacebookCircular, TiSocialInstagram } from 'react-icons/ti';
+import {
+  SlSocialReddit,
+  SlSocialTwitter,
+  SlSocialYoutube,
+} from 'react-icons/sl';
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <div className='footer-header'>
+        <div className='footer-header-left'>
+          <Logo />
+        </div>
+        <div className='footer-header-middle'>
+          <div className='footer-header-middle-left'>
+            <li className='footer-header-middle-left-menu'>
+              <FaHandPointRight />
+              <span>Posts</span>
+            </li>
+            <li className='footer-header-middle-left-menu'>
+              <FaHandPointRight />
+              <span>Questions and Answers</span>
+            </li>
+          </div>
+          <div className='footer-header-middle-right'>
+            <li className='footer-header-middle-right-menu'>
+              <FaHandPointRight />
+              <span>About us</span>
+            </li>
+            <li className='footer-header-middle-right-menu'>
+              <FaHandPointRight />
+              <span>Feedback</span>
+            </li>
+            <li className='footer-header-middle-right-menu'>
+              <FaHandPointRight />
+              <span>Help</span>
+            </li>
+          </div>
+        </div>
+        <div className='footer-header-right'>
+          <p>FOLLOW US</p>
+          <div className='footer-header-right-media'>
+            <a
+              href='#'
+              className='social'>
+              <TiSocialInstagram />
+            </a>
+            <a
+              href='#'
+              className='social'>
+              <TiSocialFacebookCircular />
+            </a>
+            <a
+              href='#'
+              className='social'>
+              <SlSocialYoutube />
+            </a>
+            <a
+              href='#'
+              className='social'>
+              <SlSocialTwitter />
+            </a>
+            <a
+              href='#'
+              className='social'>
+              <SlSocialReddit />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className='footer-footer'>
+        <p>
+          <FaCopyright />
+          {'   '}
+          Copyright. All rights reserved. Nguyen Thien Sua.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
