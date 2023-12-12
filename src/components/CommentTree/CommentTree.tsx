1ed8a6e2d99db2075aa2b6619adccc6d6117ee8a
ntsua1908 <suaapag@gmail.com>
2023-12-12 23:11:14 +0700

    Add reply feature

--- src/components/CommentTree/CommentTree.tsx
+++ src/components/CommentTree/CommentTree.tsx
@@ -1,13 +1,14 @@
-import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
-import './commentTree.scss';
+import React, { ChangeEvent, useEffect, useRef, useState } from "react";
+import "./commentTree.scss";
 import {
   TiArrowRight,
   TiArrowSortedDown,
   TiArrowSortedUp,
-} from 'react-icons/ti';
-import { SlOptionsVertical } from 'react-icons/sl';
-import VoteType from '../../enums/VoteType';
-import { Link } from 'react-router-dom';
+} from "react-icons/ti";
+import { SlOptionsVertical } from "react-icons/sl";
+import VoteType from "../../enums/VoteType";
+import { Link } from "react-router-dom";
+import { command } from "yargs";
 
 export interface Comment {
   id: string;
@@ -19,49 +20,17 @@ export interface Comment {
   upvotes: number;
   downvotes: number;
   replyFor: string; //username
-  replies?: Comment[]; // Array of nested comments
+  replies: Comment[]; // Array of nested comments
   totalReplyCount: number;
   vote: VoteType;
 }
 
 interface CommentProps {
   comment: Comment;
+  addParentComment?: (comment: Comment) => void;
 }
 
-// const Comment = ({ comment }: CommentProps) => {
-
-//   return (
-//     <div className="comment">
-//       <div className="comment-info">
-//         <img src={comment.avatar} alt="User Avatar" className="avatar" />
-//         <div className="user-details">
-//           <span className="username">{comment.username}</span>
-//           <span className="full-name">{comment.fullName}</span>
-//           <span className="date">{comment.date}</span>
-//         </div>
-//       </div>
-//       <p className="comment-text">{comment.text}</p>
-//       <div className="comment-actions">
-//         <span className="upvote">Upvote: {comment.upvotes}</span>
-//         <span className="downvote">Downvote: {comment.downvotes}</span>
-//         <button className="reply-btn" onClick={handleReplyClick}>
-//           Reply
-//         </button>
-//         <button className="report-btn">Report</button>
-//       </div>
-
-//       {comment.replies && (
-//         <div className="replies">
-//           {comment.replies.map((reply) => (
-//             <Comment key={reply.id} comment={reply} />
-//           ))}
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-const Comment = React.memo(({ comment }: CommentProps) => {
+const Comment = React.memo(({ comment, addParentComment }: CommentProps) => {
   const [isReply, setReply] = useState(false);
   const [isEdit, setEdit] = useState(false);
   const [showComment, setShowComment] = useState(false);
@@ -70,6 +39,9 @@ const Comment = React.memo(({ comment }: CommentProps) => {
   const optionRef = useRef<HTMLDivElement>(null);
   const [content, setContent] = useState(comment.text);
   const [editContent, setEditContent] = useState(comment.text);
+  const [totalCommentCount, setTotalCommentCount] = useState(
+    comment.totalReplyCount
+  );
 
   //vote state
   const [upvote, setUpvote] = useState(comment.upvotes);
@@ -77,7 +49,41 @@ const Comment = React.memo(({ comment }: CommentProps) => {
   const [vote, setVote] = useState(comment.vote);
 
   //add reply
-  const [addReply, setAddReply] = useState('');
+  const [addReply, setAddReply] = useState("");
+  const handleReply = () => {
+    // debugger;
+    const commentReply: Comment = {
+      avatar:
+        "https://i.pinimg.com/564x/94/9b/8d/949b8d8d9229693ba9d53b054b738e2a.jpg", //My avatar
+      fullName: "Nguyen Thien Sua", //My full name,
+      username: "ntsua", //My username,
+      replyFor: comment.username, //Post author user name
+      date: "2023-12-12 12:34 PM",
+      downvotes: 0,
+      upvotes: 0,
+      id: "1.4" + addReply,
+      text: addReply,
+      totalReplyCount: 0,
+      replies: [],
+      vote: VoteType.Unvote,
+    };
+    if (addParentComment) {
+      addParentComment(commentReply);
+    } else {
+      setReplies([...replies, commentReply]);
+      if (totalCommentCount == 0) {
+        setTotalCommentCount(1);
+        setShowComment(true);
+      }
+    }
+    setAddReply("");
+    setReply(false);
+  };
+
+  const handleCancelReply = () => {
+    setAddReply("");
+    setReply(false);
+  };
 
   const handleReplyClick = () => {
     setReply(!isReply);
@@ -91,7 +97,7 @@ const Comment = React.memo(({ comment }: CommentProps) => {
     setShowOption(!isShowOption);
   };
 
-  console.log(comment.id, comment.totalReplyCount);
+  console.log(comment.id);
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -103,10 +109,10 @@ const Comment = React.memo(({ comment }: CommentProps) => {
       }
     };
 
-    document.addEventListener('click', handleClickOutside);
+    document.addEventListener("click", handleClickOutside);
 
     return () => {
-      document.removeEventListener('click', handleClickOutside);
+      document.removeEventListener("click", handleClickOutside);
     };
   }, []);
 
@@ -151,22 +157,24 @@ const Comment = React.memo(({ comment }: CommentProps) => {
     }
   };
 
-  const handleAddReply = () => {};
-
-  const handleCancelReply = () => {
-    setReply(false);
+  const AddParentComment = (comment: Comment) => {
+    setReplies([...replies, comment]);
   };
 
+  useEffect(() => {
+    console.log(replies);
+  }, [replies]);
+
   return (
-    <div className='comment'>
-      <div className='comment-header'>
+    <div className="comment">
+      <div className="comment-header">
         <img
           src={comment.avatar}
-          alt='User Avatar'
-          className='comment-header-avatar'
+          alt="User Avatar"
+          className="comment-header-avatar"
         />
       </div>
-      <div className='comment-content'>
+      <div className="comment-content">
         {isEdit ? (
           <ReplyForm
             content={editContent}
@@ -175,105 +183,124 @@ const Comment = React.memo(({ comment }: CommentProps) => {
             onCancel={handleCancelEditComment}
           />
         ) : (
-          <div className='comment-content-container'>
-            <div className='comment-content-left'>
-              <div className='comment-content-details'>
-                <span className='comment-content-details-fullname'>
+          <div className="comment-content-container">
+            <div className="comment-content-left">
+              <div className="comment-content-details">
+                <span className="comment-content-details-fullname">
                   {comment.fullName}
                 </span>
-                <span className='comment-content-details-username'>
+                <span className="comment-content-details-username">
                   @{comment.username}
                 </span>
-                <span className='comment-content-details-date'>
+                <span className="comment-content-details-date">
                   {comment.date}
                 </span>
               </div>
-              <div className='comment-content-message'>
-                <Link to={`/user/profile/${'fill-later'}`}>
-                  @{comment.replyFor}
-                </Link>{' '}
-                {content}
-              </div>
-              <div className='comment-content-function'>
-                <div className='comment-content-function-votes'>
-                  <div
-                    className={`comment-content-function-votes-up ${
-                      vote == VoteType.Up && 'selected'
-                    }`}
-                    onClick={handleUpvote}>
-                    <TiArrowSortedUp />
-                    <span>{upvote}</span>
-                  </div>
-                  <div
-                    className={`comment-content-function-votes-down ${
-                      vote == VoteType.Down && 'selected'
-                    }`}
-                    onClick={handleDownvote}>
-                    <TiArrowSortedDown />
-                    <span>{downvote}</span>
-                  </div>
-                </div>
+              <div
+                className="comment-content-option"
+                ref={optionRef}
+                onClick={handleShowOption}
+              >
+                <SlOptionsVertical />
                 <div
-                  className='comment-content-function-reply'
-                  onClick={handleReplyClick}>
-                  Reply
+                  className={`comment-content-option-menu ${
+                    isShowOption && "show"
+                  }`}
+                >
+                  <ul>
+                    <li className="comment-content-option-menu-item">Report</li>
+                    {/* Validate later */}
+                    <li
+                      className="comment-content-option-menu-item"
+                      onClick={() => {
+                        setEdit(true);
+                      }}
+                    >
+                      Edit
+                    </li>
+                    <li className="comment-content-option-menu-item">Delete</li>
+                  </ul>
                 </div>
               </div>
             </div>
-            <div
-              className='comment-content-option'
-              ref={optionRef}
-              onClick={handleShowOption}>
-              <SlOptionsVertical />
+
+            <div className="comment-content-message">
+              <Link to={`/user/profile/${"fill-later"}`}>
+                @{comment.replyFor}
+              </Link>{" "}
+              {content}
+            </div>
+            <div className="comment-content-function">
+              <div className="comment-content-function-votes">
+                <div
+                  className={`comment-content-function-votes-up ${
+                    vote == VoteType.Up && "selected"
+                  }`}
+                  onClick={handleUpvote}
+                >
+                  <TiArrowSortedUp />
+                  <span>{upvote}</span>
+                </div>
+                <div
+                  className={`comment-content-function-votes-down ${
+                    vote == VoteType.Down && "selected"
+                  }`}
+                  onClick={handleDownvote}
+                >
+                  <TiArrowSortedDown />
+                  <span>{downvote}</span>
+                </div>
+              </div>
               <div
-                className={`comment-content-option-menu ${
-                  isShowOption && 'show'
-                }`}>
-                <ul>
-                  <li className='comment-content-option-menu-item'>Report</li>
-                  {/* Validate later */}
-                  <li
-                    className='comment-content-option-menu-item'
-                    onClick={() => {
-                      setEdit(true);
-                    }}>
-                    Edit
-                  </li>
-                  <li className='comment-content-option-menu-item'>Delete</li>
-                </ul>
+                className="comment-content-function-reply"
+                onClick={handleReplyClick}
+              >
+                Reply
               </div>
             </div>
           </div>
         )}
-        {comment.totalReplyCount > 0 && (
+        {totalCommentCount > 0 && (
           <div
-            className='comment-content-showComment'
-            onClick={handleShowReplyClick}>
+            className="comment-content-showComment"
+            onClick={handleShowReplyClick}
+          >
             <div
               className={`comment-content-showComment-button ${
-                showComment && 'show'
-              }`}>
+                showComment && "show"
+              }`}
+            >
               <TiArrowRight />
             </div>
-            <div className='comment-content-showComment-label'>
+            <div className="comment-content-showComment-label">
               Show replies
             </div>
           </div>
         )}
 
         {isReply && (
-          <ReplyForm
-            content={editContent}
-            setContent={setEditContent}
-            onSave={handleSaveEditComment}
-            onCancel={handleCancelEditComment}
-          />
+          <div className="comment-content-form">
+            <ReplyForm
+              content={addReply}
+              setContent={setAddReply}
+              onSave={handleReply}
+              onCancel={handleCancelReply}
+            />
+          </div>
         )}
         <div
           className={`comment-content-replies ${
-            showComment && replies && 'show'
-          }`}>
-          {replies && replies.map((reply) => <Comment comment={reply} />)}
+            showComment && replies && "show"
+          }`}
+        >
+          {replies &&
+            replies.map((reply) => (
+              <Comment
+                key={reply.id}
+                comment={reply}
+                addParentComment={AddParentComment}
+              />
+            ))}
         </div>
       </div>
     </div>
@@ -295,26 +322,30 @@ export const ReplyForm: React.FC<ReplyFormProps> = ({
 }) => {
   const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setContent(e.currentTarget.value);
-    console.log(e.currentTarget.value, e.currentTarget.value.length === 0);
+    // console.log(e.currentTarget.value, e.currentTarget.value.length === 0);
   };
 
   return (
-    <div className='comment-reply-form'>
+    <div className="comment-reply-form">
       <textarea
-        className='comment-reply-form-input'
+        autoFocus
+        className="comment-reply-form-input"
         value={content}
         onChange={handleChange}
+        placeholder="Write your comment..."
       />
-      <div className='comment-reply-form-function'>
+      <div className="comment-reply-form-function">
         <button
-          className='comment-reply-form-function-save'
+          className="comment-reply-form-function-save"
           onClick={onSave}
-          disabled={content.length === 0}>
+          disabled={content.length === 0}
+        >
           Save changes
         </button>
         <button
-          className='comment-reply-form-function-cancel'
-          onClick={onCancel}>
+          className="comment-reply-form-function-cancel"
+          onClick={onCancel}
+        >
           Cancel
         </button>
       </div>
@@ -327,14 +358,51 @@ interface CommentTreeProps {
 }
 
 const CommentsTree = ({ comments }: CommentTreeProps) => {
+  const [comment, setComment] = useState(comments);
+  const [reply, setReply] = useState("");
+
+  const handleOnAddComment = () => {
+    // console.log(reply);
+    setReply("");
+    setComment([
+      {
+        avatar:
+          "https://i.pinimg.com/564x/94/9b/8d/949b8d8d9229693ba9d53b054b738e2a.jpg", //My avatar
+        fullName: "Nguyen Thien Sua", //My full name,
+        username: "ntsua", //My username,
+        replyFor: "Haley", //Post author user name
+        date: "2023-12-12 12:34 PM",
+        downvotes: 0,
+        upvotes: 0,
+        id: "1.4" + reply,
+        text: reply,
+        totalReplyCount: 0,
+        replies: [],
+        vote: VoteType.Unvote,
+      },
+      ...comment,
+    ]);
+  };
+
+  const handleOnCancelComment = () => {
+    setReply("");
+  };
+
   return (
-    <div className='comments-tree'>
-      {comments.map((comment) => (
-        <Comment
-          key={comment.id}
-          comment={comment}
+    <div className="comments-tree">
+      <div className="comments-tree-new">
+        <ReplyForm
+          content={reply}
+          setContent={setReply}
+          onSave={handleOnAddComment}
+          onCancel={handleOnCancelComment}
         />
-      ))}
+      </div>
+      <div className="comments-tree-list">
+        {comment.map((comment) => (
+          <Comment key={comment.id} comment={comment} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/pages/Post/Post.tsx
+++ src/pages/Post/Post.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import PostContent from '../../components/PostContent/PostContent';
-import Header from '../../components/Header/Header';
-import Footer from '../../components/Footer/Footer';
-import VoteType from '../../enums/VoteType';
-import CommentsTree from '../../components/CommentTree/CommentTree';
-import { Comment } from '../../components/CommentTree/CommentTree';
-import './post.scss';
+import React, { useState } from "react";
+import PostContent from "../../components/PostContent/PostContent";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import VoteType from "../../enums/VoteType";
+import CommentsTree from "../../components/CommentTree/CommentTree";
+import { Comment } from "../../components/CommentTree/CommentTree";
+import "./post.scss";
 
 const value = `
 ## Horizontal Rules
@@ -258,28 +258,28 @@ It converts "HTML", but keep intact partial entries like "xxxHTMLyyy" and so on.
 
 const comments: Comment[] = [
   {
-    id: '1',
-    avatar: 'https://stardewvalleywiki.com/mediawiki/images/0/04/Alex.png',
-    username: 'alex',
-    fullName: 'Alex',
-    date: '2023-12-05  13:34 PM',
-    text: 'This is the first comment.',
+    id: "1",
+    avatar: "https://stardewvalleywiki.com/mediawiki/images/0/04/Alex.png",
+    username: "alex",
+    fullName: "Alex",
+    date: "2023-12-05  13:34 PM",
+    text: "This is the first comment.",
     upvotes: 10,
     downvotes: 2,
-    replyFor: 'Haley',
+    replyFor: "Haley",
     totalReplyCount: 3,
     vote: VoteType.Down,
     replies: [
       {
-        id: '1.1',
-        avatar: 'https://stardewvalleywiki.com/mediawiki/images/2/28/Emily.png',
-        username: 'amily',
-        fullName: 'Amily',
-        date: '2023-12-06  13:34 PM',
-        text: 'Reply to the first comment. Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus sunt commodi possimus odio, officia accusantium nemo nobis eaque odit beatae aperiam dolore doloribus ullam quisquam omnis numquam ratione libero in.',
+        id: "1.1",
+        avatar: "https://stardewvalleywiki.com/mediawiki/images/2/28/Emily.png",
+        username: "amily",
+        fullName: "Amily",
+        date: "2023-12-06  13:34 PM",
+        text: "Reply to the first comment. Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus sunt commodi possimus odio, officia accusantium nemo nobis eaque odit beatae aperiam dolore doloribus ullam quisquam omnis numquam ratione libero in.",
         upvotes: 5,
         downvotes: 1,
-        replyFor: 'alex',
+        replyFor: "alex",
         totalReplyCount: 0,
         vote: VoteType.Up,
         replies: [
@@ -287,48 +287,49 @@ const comments: Comment[] = [
         ],
       },
       {
-        id: '1.2',
-        avatar: 'https://stardewvalleywiki.com/mediawiki/images/1/1b/Robin.png',
-        username: 'robin',
-        fullName: 'Robin',
-        date: '2023-12-07  13:34 PM',
-        text: 'This is the second comment.',
+        id: "1.2",
+        avatar: "https://stardewvalleywiki.com/mediawiki/images/1/1b/Robin.png",
+        username: "robin",
+        fullName: "Robin",
+        date: "2023-12-07  13:34 PM",
+        text: "This is the second comment.",
         upvotes: 8,
         downvotes: 0,
-        replyFor: 'alex',
+        replyFor: "alex",
         totalReplyCount: 0,
         vote: VoteType.Unvote,
-        // No replies for the second comment
+        replies: [],
       },
       {
-        id: '1.1.1',
-        avatar: 'https://stardewvalleywiki.com/mediawiki/images/0/04/Alex.png',
-        username: 'alex',
-        fullName: 'Alex',
-        date: '2023-12-06  13:34 PM',
-        text: 'Reply to the first comment.',
-        replyFor: 'robin',
+        id: "1.1.1",
+        avatar: "https://stardewvalleywiki.com/mediawiki/images/0/04/Alex.png",
+        username: "alex",
+        fullName: "Alex",
+        date: "2023-12-06  13:34 PM",
+        text: "Reply to the first comment.",
+        replyFor: "robin",
         upvotes: 5,
         downvotes: 1,
         totalReplyCount: 0,
         vote: VoteType.Unvote,
+        replies: [],
       },
       // Add more replies as needed
     ],
   },
   {
-    id: '2',
-    avatar: 'https://stardewvalleywiki.com/mediawiki/images/2/2b/Lewis.png',
-    username: 'lewis',
-    fullName: 'Lewis',
-    date: '2023-12-07 13:34 PM',
-    text: 'This is the second comment.',
+    id: "2",
+    avatar: "https://stardewvalleywiki.com/mediawiki/images/2/2b/Lewis.png",
+    username: "lewis",
+    fullName: "Lewis",
+    date: "2023-12-07 13:34 PM",
+    text: "This is the second comment.",
     upvotes: 8,
-    replyFor: 'alex',
+    replyFor: "alex",
     downvotes: 0,
     totalReplyCount: 0,
     vote: VoteType.Up,
-    // No replies for the second comment
+    replies: [],
   },
   // Add more comments as needed
 ];
@@ -355,13 +356,13 @@ function Post() {
   const [data, setData] = useState<PostProps>({
     content: value,
     thumbnail:
-      'https://1.bp.blogspot.com/-E6gB3SWavGE/X4Wo-00m4qI/AAAAAAAAFMo/VaTo7SBekpgcy9iDDk9j108npXiSWWRPwCLcBGAsYHQ/w0/How%2Bto%2Bfix%2Bthumbnail%2Bissue%2Bin%2Bblogger%2Bhomepage.png',
+      "https://1.bp.blogspot.com/-E6gB3SWavGE/X4Wo-00m4qI/AAAAAAAAFMo/VaTo7SBekpgcy9iDDk9j108npXiSWWRPwCLcBGAsYHQ/w0/How%2Bto%2Bfix%2Bthumbnail%2Bissue%2Bin%2Bblogger%2Bhomepage.png",
     excerpt:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam aliquam quos voluptate laborum asperiores rem. Inventore labore excepturi tempora nam impedit veniam rerum laboriosam, reprehenderit vero voluptas ratione, perspiciatis hic.',
-    avatar: 'https://stardewvalleywiki.com/mediawiki/images/1/1b/Haley.png',
-    fullname: 'Halley',
-    username: 'Halley',
-    date: '08-12-2023 16:58 PM',
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam aliquam quos voluptate laborum asperiores rem. Inventore labore excepturi tempora nam impedit veniam rerum laboriosam, reprehenderit vero voluptas ratione, perspiciatis hic.",
+    avatar: "https://stardewvalleywiki.com/mediawiki/images/1/1b/Haley.png",
+    fullname: "Halley",
+    username: "Halley",
+    date: "08-12-2023 16:58 PM",
     viewCount: 2134,
     postCount: 11,
     followCount: 342,
@@ -369,12 +370,12 @@ function Post() {
     comments: comments,
     isMyPost: true,
     vote: VoteType.Up,
-    tags: ['markdown', 'blog', 'introduce', 'demo', 'guide'],
+    tags: ["markdown", "blog", "introduce", "demo", "guide"],
   });
 
   return (
-    <div className='post'>
-      <div className='post-content'>
+    <div className="post">
+      <div className="post-content">
         <PostContent
           content={data.content}
           thumbnail={data.thumbnail}
@@ -393,8 +394,8 @@ function Post() {
           tags={data.tags}
         />
       </div>
-      <div className='post-comment'>
-        <h2 className='post-comment-title'>Comments</h2>
+      <div className="post-comment">
+        <h2 className="post-comment-title">Comments</h2>
         <CommentsTree comments={data.comments} />
       </div>
     </div>
